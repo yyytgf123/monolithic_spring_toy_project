6819903f3fcf6fb4f19d6745e68b561c94062206
dev123 <hyunho3445@gmail.com>
2025-09-02 16:33:56 +0900

    spring initializr

---
+++ toy_project/src/test/java/toy_project/toy_project/ToyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package toy_project.toy_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
